070f6e8e1126ba7530ae61cae437fa05aef78da1
lhx <2900550329@qq.com>
2019-03-16 11:56:57 +0800

    springBoot-jsp

---
+++ springBoot 的web项目/springboot_jsp/src/main/java/com/example/springboot_jsp/IndexController.java
@@ -0,0 +1,14 @@
+package com.example.springboot_jsp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index(Model model){
+        model.addAttribute("name", "刘海霞");
+        return "index";
+    }
+}
