2168430dfe1c5303b9650196419edbc37493385e
lhx <2900550329@qq.com>
2019-03-26 14:01:59 +0800

    springboot-邮件发送项目

---
+++ springBoot 的web项目/spring-boot-starter-mail/src/main/java/com/example/springbootstartermail/SpringBootStarterMailApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootstartermail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStarterMailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootStarterMailApplication.class, args);
+    }
+
+}
